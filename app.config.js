6ae8fac675f7e17462dfc8676c52bcec930ac99d
dexbotsdev <123926754+dexbotsdev@users.noreply.github.com>
2025-07-31 13:00:04 +0530

    Fix missing comma in app.config.js

---
+++ app.config.js
@@ -0,0 +1,47 @@
+import reactRefresh from "@vitejs/plugin-react";
+import { createApp } from "vinxi";
+
+export default createApp({
+	server: {
+		experimental: {
+			websocket: true,
+		},
+	},
+	routers: [
+		{
+			name: "public",
+			type: "static",
+			dir: "./public",
+			base: "/",
+		},
+		{
+			name: "client",
+			type: "client",
+			handler: "./app/client.tsx",
+			target: "browser",
+			base: "/_build",
+			plugins: () => [],
+		}
+		{
+			name: "websocket",
+			type: "http",
+			handler: "./app/ws.ts",
+			target: "server",
+			base: "/_ws",
+		},
+		{
+			name: "party",
+			type: "http",
+			handler: "./app/party.ts",
+			target: "server",
+			base: "/party",
+		},
+		{
+			name: "ssr",
+			type: "http",
+			middleware: "./app/middleware.tsx",
+			handler: "./app/server.tsx",
+			target: "server",
+		},
+	],
+});
